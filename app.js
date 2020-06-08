682f7eaca76d68aa4f001d33dfb0facf58b5512e
Paul Desiles <pauldesiles@gmail.com>
2020-06-08 12:26:05 +0200

    cors : allow frontend server calls

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const helmet = require('helmet')
+const helmet = require('helmet');
+const cors = require('cors');
 const idRouter = require('./routes/idRouter');
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
+// Helmet
 const app = express();
 app.use(helmet());
 app.use(helmet.contentSecurityPolicy({
@@ -14,7 +16,24 @@ app.use(helmet.contentSecurityPolicy({
   }
 }));
 
+// Cors
+var allowedOrigins = [];
+if (process.env.CORSALLOWED){
+	allowedOrigins = process.env.CORSALLOWED.split(',');
+}
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error(`${origin} not allowed by CORS`));
+    }
+  }
+}));
+
+// Routes
 app.use('/generateId', idRouter);
+
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`listening on port ${port}`));
